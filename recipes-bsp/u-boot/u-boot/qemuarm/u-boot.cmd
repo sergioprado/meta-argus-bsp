d91a39d724115c0193f2e947f23a5ef7a4cebda9
Sergio Prado <sergio.prado@e-labworks.com>
2023-11-07 13:19:22 -0300

    qemuarm: boot with a ramdisk
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>

--- recipes-bsp/u-boot/u-boot/qemuarm/u-boot.cmd
+++ recipes-bsp/u-boot/u-boot/qemuarm/u-boot.cmd
@@ -1,11 +1,13 @@
 env set kernel_image "/zImage"
 env set dtb_image "/qemuarm.dtb"
+env set ramdisk_image "/ramdisk.img"
 
-env set bootargs "console=ttyAMA0 console=tty1 root=/dev/vda2 rootfstype=squashfs"
+env set bootargs "console=tty1 console=ttyAMA0 root=/dev/vda2 rootfstype=squashfs ${bootargs_argus}"
 
 env set load_kernel 'ext4load ${devtype} ${devnum}:${distro_bootpart} ${kernel_addr_r} ${kernel_image}'
 env set load_dtb 'ext4load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr} ${dtb_image}'
+env set load_ramdisk 'ext4load ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} ${ramdisk_image}; setenv ramdisk_size 0x${filesize}'
 
-env set bootcmd_run 'run load_kernel; run load_dtb; bootz ${kernel_addr_r} - ${fdt_addr}'
+env set bootcmd_run 'run load_kernel; run load_dtb; run load_ramdisk; bootz ${kernel_addr_r} ${ramdisk_addr_r}:${ramdisk_size} ${fdt_addr}'
 
 run bootcmd_run
