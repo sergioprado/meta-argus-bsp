0e3e265b2fa7f963b8512751973b659d25948900
Sergio Prado <sergio.prado@e-labworks.com>
2023-11-08 11:50:39 -0300

    qemuarm: boot with a FIT image
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>

--- recipes-bsp/u-boot/u-boot/qemuarm/u-boot.cmd
+++ recipes-bsp/u-boot/u-boot/qemuarm/u-boot.cmd
@@ -1,13 +1,10 @@
-env set kernel_image "/zImage"
-env set dtb_image "/qemuarm.dtb"
-env set ramdisk_image "/ramdisk.img"
+env set fit_image "/fitImage"
+env set fit_addr_r "0x48000000"
 
 env set bootargs "console=tty1 console=ttyAMA0 root=/dev/vda2 rootfstype=squashfs ${bootargs_argus}"
 
-env set load_kernel 'ext4load ${devtype} ${devnum}:${distro_bootpart} ${kernel_addr_r} ${kernel_image}'
-env set load_dtb 'ext4load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr} ${dtb_image}'
-env set load_ramdisk 'ext4load ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} ${ramdisk_image}; setenv ramdisk_size 0x${filesize}'
+env set load_fit 'ext4load ${devtype} ${devnum}:${distro_bootpart} ${fit_addr_r} ${fit_image}'
 
-env set bootcmd_run 'run load_kernel; run load_dtb; run load_ramdisk; bootz ${kernel_addr_r} ${ramdisk_addr_r}:${ramdisk_size} ${fdt_addr}'
+env set bootcmd_run 'run load_fit; bootm ${fit_addr_r}'
 
 run bootcmd_run
